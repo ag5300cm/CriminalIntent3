5996c15edbdf3516400c60de3efff11548506494
Benjamin <blabblabbalab@gmail.com>
2016-10-04 01:24:20 -0500

    Done Chapter 13

--- app/src/main/java/com/example/meghan/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/meghan/criminalintent/CrimeListFragment.java
@@ -3,6 +3,7 @@ package com.example.meghan.criminalintent;
 import android.content.Intent; //added by page 193 for summoning another fragemnt
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -27,8 +28,12 @@ import java.util.List;
 // page 174, 175
 public class CrimeListFragment  extends Fragment { //page 181
 
+    private static final String SAVED_SUBTITLE_VISIBLE = "subtitle"; //page 254, for saving subtitle visability
+
     private RecyclerView mCrimeRecyclerView;
     private CrimeAdapter mAdapter;
+
+    private boolean mSubtitleVisible; //page 251, Adding a varible to keep track of subtitle visibility
 
     @Override //page 244, 245 This is responsible for calling Fragment.onCreateOptionsMenu(Menu, MenuInflater)
                // when the activity receives its onCreateOptionsMenu(_) callback from the OS.
@@ -47,6 +52,10 @@ public class CrimeListFragment  extends Fragment { //page 181
         mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity())); //RecyclerView need a layoutManager to work
         //LayoutManager handles the positions of items and scrolling behavior
 
+        if (savedInstancesState != null) { //page 254, for saving subtitle visibility
+            mSubtitleVisible = savedInstancesState.getBoolean(SAVED_SUBTITLE_VISIBLE);
+        }
+
         updateUI();
 
         return view;
@@ -58,27 +67,61 @@ public class CrimeListFragment  extends Fragment { //page 181
         updateUI(); //should add any changes to the list.
     }
 
+    @Override //page 254, subtitle should appear in recreated views
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(SAVED_SUBTITLE_VISIBLE, mSubtitleVisible);
+    }
+
     @Override //page 244, when
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         //when you call MenuInflater.inflate(int, menu) and pass in the resource ID of your menu file. This populates
             //the menu instance with the items defined in your file
         super.onCreateOptionsMenu(menu, inflater); //superclass implementation, that way any fuctionality defined by the superclass will still work.
-        inflater.inflate(R.menu.fragment_crime_list, menu);
+        inflater.inflate(R.menu.fragment_crime_list, menu); //page 244,
+
+        MenuItem subtitleItem = menu.findItem(R.id.menu_item_show_subtitle); //page 252, 251, used to show and hide the subtitle
+        if (mSubtitleVisible) {
+            subtitleItem.setTitle(R.string.hide_subtitle);
+        } else {
+            subtitleItem.setTitle(R.string.show_subtitle);
+        }
     }
 
     @Override //page 248
     public boolean onOptionsItemSelected(MenuItem item) { //getting Toolbar menu to bring up where you write in crimes
-        switch (item.getItemId()) {
+        switch (item.getItemId()) {//page 248
             case R.id.menu_item_new_crime:
                 Crime crime = new Crime(); //Creating a new crime
                 CrimeLab.get(getActivity()).addCrime(crime);  //adding the crime to the list
                 Intent intent = CrimePagerActivity
                         .newIntent(getActivity(), crime.getId());
                 startActivity(intent);
-                return true; //Should return true to show no further processing is necessary
-            default:
+                return true; //Should return true to show no further processing is necessary //page 248
+            case R.id.menu_item_show_subtitle: //page 251, callin the method when the user presses the new action item.
+                mSubtitleVisible = !mSubtitleVisible; //page 252, used to show and hide subtitle in toolbar.
+                getActivity().invalidateOptionsMenu(); //page 252
+                updateSubtitle(); //page 251
+                return true;
+            default://page 248
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    private void updateSubtitle() { //page 250, The subtitle will display the number of crimes in CriminalIntent
+        CrimeLab crimeLab = CrimeLab.get(getActivity());  //page 250
+        int crimeCount = crimeLab.getCrimes().size();
+        String subtitle = getString(R.string.subtitle_format, crimeCount); //first generates the subtitle string using getString(int resId, Object_
+                // formateArgs) method, which accepts replacement values for ethe placeholders in the string resource.  //page 250
+
+        if (!mSubtitleVisible) { //page 252, To make subtitle disappear.
+            subtitle = null;
+        }
+
+        //page 250, Next, the activity that is hosing the CrimeListFragment is cast ot an AppCompatActivity. CriminalIntent uses the AppCompat library
+        //so all activities will be subclass of AppCompatActivity which allow you to access the toolbar (also know as "action bar" for legacy reasons).
+        AppCompatActivity activity = (AppCompatActivity) getActivity();  //page 250
+        activity.getSupportActionBar().setSubtitle(subtitle);
     }
 
     private void updateUI() { //Sets up CrimeListFragment's user interface, page 183, 184
@@ -92,6 +135,7 @@ public class CrimeListFragment  extends Fragment { //page 181
             mAdapter.notifyDataSetChanged(); //for changes made
         }
 
+        updateSubtitle(); //page 253, will prevent the text from disappearing when returning to CrimeListFragment.
     }
 
     private class CrimeHolder extends RecyclerView.ViewHolder //code for list, page 182
